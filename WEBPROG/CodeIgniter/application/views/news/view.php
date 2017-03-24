61e10f451be8fb7ba6ef3a91af545ff946032fdb
Lester Hernandez <janlesterhernandez@gmail.com>
2017-03-24 11:02:12 +0800

    Added CodeIgniter Exercise

---
+++ WEBPROG/CodeIgniter/application/views/news/view.php
@@ -0,0 +1,4 @@
+<?php
+echo '<h2>'.$news_item['CompleteName'].'</h2>';
+echo '<strong>'.'Nickname: '.'</strong>'.$news_item['Nickname'], '<br/>'.'<strong>'.'Email Address: '.'</strong>'.$news_item['Email'], '<br/>'.'<strong>'.'Home Address: '.'</strong>'.$news_item['HomeAddress'], '<br/>'.'<strong>'.'Gender: '.'</strong>'.$news_item['gender'], '<br/>'.'<strong>'.'Cellphone Number: '.'</strong>'.$news_item['CellphoneNumber'], '<br/>'.'<strong>'.'Comments: '.'</strong>'.'<br/>'.$news_item['comments'];
+        
