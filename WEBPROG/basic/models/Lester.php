53e0b0e4ad52b38574de3c6ce027e45fb4150d19
Lester Hernandez <janlesterhernandez@gmail.com>
2017-04-04 10:30:58 +0800

    Added All Part 1 Yii2 Exercises

---
+++ WEBPROG/basic/models/Lester.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property integer $id
+ * @property string $slug
+ * @property string $CompleteName
+ * @property string $Nickname
+ * @property string $Email
+ * @property string $HomeAddress
+ * @property string $gender
+ * @property integer $CellphoneNumber
+ * @property string $comments
+ */
+class Lester extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['slug', 'CompleteName', 'Nickname', 'Email', 'HomeAddress', 'gender', 'CellphoneNumber', 'comments'], 'required'],
+            [['CellphoneNumber'], 'integer'],
+            [['slug', 'CompleteName', 'Nickname', 'Email', 'HomeAddress', 'gender'], 'string', 'max' => 128],
+            [['comments'], 'string', 'max' => 200],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'slug' => 'Slug',
+            'CompleteName' => 'Complete Name',
+            'Nickname' => 'Nickname',
+            'Email' => 'Email',
+            'HomeAddress' => 'Home Address',
+            'gender' => 'Gender',
+            'CellphoneNumber' => 'Cellphone Number',
+            'comments' => 'Comments',
+        ];
+    }
+}
