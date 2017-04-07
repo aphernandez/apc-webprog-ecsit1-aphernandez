08b0d9ac23565412258dc34b96469bc0959ce757
Lester Hernandez <janlesterhernandez@gmail.com>
2017-04-07 10:41:48 +0800

    Added trivia and update profile

--- WEBPROG/basic/views/trivia/_form.php
+++ WEBPROG/basic/views/trivia/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Profile;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Trivia */
@@ -18,7 +20,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'answer')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'profile_id')->textInput() ?>
+    <?= $form->field($model, 'profile_id')->dropDownList(ArrayHelper::map(Profile::find()->all(), 'id', 'CompleteName'), ['prompt'=>'Choose']); ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
